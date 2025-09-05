e19392a1d8d6a21e5d7eca0bcda57048eeb29bc0
Broninn <bruno-hs@outlook.com>
2025-09-05 07:57:18 -0300

    getting params routes

--- src/controllers/tickets/index.js
+++ src/controllers/tickets/index.js
@@ -1,4 +1,6 @@
 export function index({req,res,database}){
+    const {status } = req.query
+    console.log(status)
     const tickets = database.select("tickets")
     return res.end(JSON.stringify(tickets))
 }
