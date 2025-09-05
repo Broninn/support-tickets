b85960bb239b2edcb5e2602fb26c1c78bf0e8190
Broninn <bruno-hs@outlook.com>
2025-09-05 07:51:02 -0300

    dynamic params

--- src/middlewares/routeHandler.js
+++ src/middlewares/routeHandler.js
@@ -5,12 +5,12 @@ const database = new Database();
 
 export function routeHandler(req, res) {
   const route = routes.find((route) => {
-    return route.method === req.method && route.path === req.url;
+    return route.method === req.method && route.path.test(req.url);
   });
 
-  if(route){
-    return route.controller({req, res, database});
-  }else{
+  if (route) {
+    return route.controller({ req, res, database });
+  } else {
     res.writeHead(404).end("Rota não encontrada");
   }
 }
