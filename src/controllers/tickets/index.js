2055a99637bf1052415801c8f82fc60b9a150a59
Broninn <bruno-hs@outlook.com>
2025-09-17 13:37:21 -0300

    filter by status

--- src/controllers/tickets/index.js
+++ src/controllers/tickets/index.js
@@ -1,6 +1,6 @@
-export function index({req,res,database}){
-    const {status } = req.query
-    console.log(status)
-    const tickets = database.select("tickets")
-    return res.end(JSON.stringify(tickets))
+export function index({ req, res, database }) {
+  const { status } = req.query;
+  const filters = status ? { status } : null;
+  const tickets = database.select("tickets", filters);
+  return res.end(JSON.stringify(tickets));
 }
