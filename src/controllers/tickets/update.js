7563c6bcb9efbc939d453b689cf8d070c83e1e6a
Broninn <bruno-hs@outlook.com>
2025-09-25 13:16:12 -0300

    updating ticket

--- src/controllers/tickets/update.js
+++ src/controllers/tickets/update.js
@@ -1,3 +1,12 @@
-export function update({req, res, database}){
-    res.end("OK")
+export function update({ req, res, database }) {
+  const { id } = req.params;
+  const { equipment, description } = req.body;
+
+  database.update("tickets", id, {
+    equipment,
+    description,
+    updated_at: new Date(),
+  });
+
+  return res.end();
 }
